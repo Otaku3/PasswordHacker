8dbe385e32e3ac4ed425fdb018cbe5104787c285
Otaku3 <Otakumi@m.cs.osakafu-u.ac.jp>
2018-06-05 03:23:11 +0900

    textbook finish

--- PasswordHacker/ViewController.swift
+++ PasswordHacker/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var resultLabel1: UILabel!    //右から1
+    @IBOutlet var resultLabel2: UILabel!    //右から2
+    @IBOutlet var resultLabel3: UILabel!    //右から3
+    @IBOutlet var resultLabel4: UILabel!    //右から4
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    var password: Int = 8000
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +27,42 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func start(){
+        for i in 0...9999{
+            //解析中の数字を表示
+            countLabel.text = String(i)
+            //処理を待機（UX的に）
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.0005))
+            
+            if password == i {
+                //passwordの桁ごとの数字を入れる配列
+                var digits = [Int]()
+                
+                for _ in 0...3{
+                    digits.append(password%10)
+                    password = password / 10
+                }
+                resultLabel1.text = String(digits[0])
+                resultLabel2.text = String(digits[1])
+                resultLabel3.text = String(digits[2])
+                resultLabel4.text = String(digits[3])
+                
+                print("正解は\(i)です！")
+                countLabel.text = "正解は\(i)です！"
+                break
+            }
+        }
+    }
+    
+    @IBAction func reset(){
+        password = 0084
+        countLabel.text = "「START」ボタンを押して解析開始"
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
     }
 
 
